fc60353539f3eb3937387aea179fcf5e51b41677
多啦b梦 <1715109585@qq.com>
2023-11-03 16:49:20 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,17 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import IconsResolver from 'unplugin-icons/resolver'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
         host: "0.0.0.0",
+        port: 3000,
         hmr: {
             host: 'localhost',
             protocol: 'ws',
@@ -15,11 +22,42 @@ export default defineConfig({
     publicDir: "public",
     plugins: [
         vue(),
+        vueJsx(),
+        AutoImport({
+            imports: ['vue'],
+            resolvers: [
+                ElementPlusResolver(),
+
+                // Auto import icon components
+                // 自动导入图标组件
+                IconsResolver({
+                    prefix: 'Icon',
+                }),
+            ],
+        }),
+        Components({
+            resolvers: [
+                // Auto register icon components
+                // 自动注册图标组件
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                }),
+                // Auto register Element Plus components
+                // 自动导入 Element Plus 组件
+                ElementPlusResolver(),
+            ],
+        }),
+
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    esbuild: {
+        jsxFactory: 'h',
+        jsxFragment: 'Fragment',
+        // jsxInject: `import React from 'react'`,
     },
     optimizeDeps: {
         include: [
