516439a2f1b6626a8cd0290c92a84ea0835b1129
多啦b梦 <1715109585@qq.com>
2025-05-06 13:55:54 +0800

    1

--- frontend/src/components/boxs/el/ProgressBar/ProgressBar.vue
+++ frontend/src/components/boxs/el/ProgressBar/ProgressBar.vue
@@ -1,26 +1,29 @@
 <template>
-    <el-slider
-        :style="item.size=='custom' ? getItemStyle(item):''"
-        style="width: 80%;margin:0px 20px"
-        v-model="item.n"
-               :size="item.size=='custom' ?'':item.size"
-               :min="item.min"
-               :max="item.max"
-               :step="item.step"
-    />
+  <el-slider
+      :style="item.size=='custom' ? getItemStyle(item):''"
+      style="width: 80%;margin:0px 20px"
+      v-model="item.n"
+      :size="item.size=='custom' ?'':item.size"
+      :min="item.min"
+      :max="item.max"
+      :step="item.step"
+      :format-tooltip="formatTooltip"
+  />
 
 </template>
 
-<script>
+<script setup>
 import {getItemStyle} from "@/public";
 
-export default {
-  methods: {getItemStyle},
-  props: {
-    item: {
-      type: Object,
-      default: '',
-    }
-  },
+import {defineEmits, defineProps} from "vue";
+
+const {item} = defineProps(['item'])
+console.log("自定义组件数据", item);
+const emits = defineEmits(["CustomEvent"]);
+console.log("自定义组件事件", emits);
+
+function formatTooltip(index){
+  return index
 }
+
 </script>
